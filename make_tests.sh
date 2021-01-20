8034003eea5c6d883f05483cafd2a80e647d1c58
Ronan Barzic <ronan.barzic@onio.com>
2021-01-20 22:51:11 +0100

    Fix make_tests.sh regarding output dir

--- make_tests.sh
+++ make_tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #TESTRUNS=5
-OUTPUTFILE=../../tmp_tests/test #Path and name
+OUTPUTFILE=tmp_tests/test #Path and name
 CONFIGFILE=config/nanorv32IM.config
 
+mkdir -p output/$OUTPUTFILE
 #Generate test
 java -Xmx1G -Xss8M -XX:MaxPermSize=128M -jar sbt-launch.jar "generator/run -C $CONFIGFILE -o $OUTPUTFILE -n $1"
